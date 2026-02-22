807c0c7f64bf4dc3fcae48fc626c7ea01757200e
kuliran <137825720+kuliran@users.noreply.github.com>
2026-02-22 12:35:03 +0300

    refactor(write-service): rm pastes.metadata partition - useless

--- db/postgresql/migrations/V001__init.sql
+++ db/postgresql/migrations/V001__init.sql
@@ -1,33 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS pastes;
 
 CREATE TABLE IF NOT EXISTS pastes.metadata (
-    id VARCHAR(64),
+    id VARCHAR(64) PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL,
     expires_at TIMESTAMPTZ NOT NULL,
     delete_key TEXT NOT NULL,
-    size_bytes INTEGER NOT NULL,
-
-    PRIMARY KEY (id, expires_at)
-) PARTITION BY RANGE (expires_at);
-
-CREATE SCHEMA IF NOT EXISTS partman;
-CREATE EXTENSION IF NOT EXISTS pg_partman SCHEMA partman;
-
-SELECT proargnames, pg_get_function_arguments(oid)
-FROM pg_proc
-WHERE proname = 'create_parent'
-AND pronamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'partman');
-
-SELECT partman.create_parent(
-    p_parent_table  => 'pastes.metadata',
-    p_control       => 'expires_at',
-    p_interval      => '1 week',
-    p_premake       => 4
+    size_bytes INTEGER NOT NULL
 );
-
-SELECT partman.run_maintenance('pastes.metadata');
-
-UPDATE partman.part_config
-SET    retention = '1 week',
-       retention_keep_table = false
-WHERE  parent_table = 'pastes.metadata';
