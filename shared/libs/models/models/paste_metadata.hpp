0cfdd1ac0dca99c1c846661033e133660f333773
kuliran <137825720+kuliran@users.noreply.github.com>
2026-02-26 00:02:11 +0300

    chore: rm unused shared/models/paste_blob

--- shared/libs/models/models/paste_metadata.hpp
+++ shared/libs/models/models/paste_metadata.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <userver/storages/postgres/io/chrono.hpp>
+#include <userver/storages/postgres/io/enum_types.hpp>
 
 struct PasteMetadata {
     std::string id;
@@ -9,4 +10,16 @@ struct PasteMetadata {
     userver::storages::postgres::TimePointTz created_at;
     userver::storages::postgres::TimePointTz expires_at;
     int size_bytes;
+};
+
+enum class PasteStatus { kPending, kSubmitted, kSoftDeleted };
+
+template <>
+struct userver::storages::postgres::io::CppToUserPg<PasteStatus> : userver::storages::postgres::io::EnumMappingBase<PasteStatus> {
+    static constexpr DBTypeName postgres_name = "pastes.status";
+    static constexpr Enumerator enumerators[]{
+        {PasteStatus::kPending, "pending"},
+        {PasteStatus::kSubmitted, "submitted"},
+        {PasteStatus::kSoftDeleted, "deleted"},
+    };
 };
