6b6b0acda4103e11e0aa80fcaa861163043b0dc2
kuliran <137825720+kuliran@users.noreply.github.com>
2026-02-26 23:04:27 +0300

    test(read-service): presigned url

--- services/read-service/tests/fixtures/api_get_paste.py
+++ services/read-service/tests/fixtures/api_get_paste.py
@@ -1,34 +1,67 @@
 import pytest
 from dataclasses import dataclass
-from datetime import datetime
 from dateutil.parser import isoparse
 from shared.fixtures.raw_get_paste import GetPasteResult
+from datetime import datetime
+import shared.utils.auth as auth
+
+@dataclass
+class GetPasteUrlResult:
+    presigned_url: str
+    size_bytes: int
+    created_at_utc: datetime
+    expires_at_utc: datetime
 
 @pytest.fixture
-async def api_get_paste(service_client) -> GetPasteResult:
-    async def _get(paste_id: str):
-        response = await service_client.get(f'/api/v1/{paste_id}')
+async def api_get_paste_url(auth_client, endpoints) -> GetPasteUrlResult:
+    async def _get(paste_id: str, *, client: auth.Client = auth_client):
+        response = await client.get(endpoints['get_paste_presigned_url'] + f'/{paste_id}')
         assert response.status == 200
         assert 'application/json' in response.headers['Content-Type']
 
         json = response.json()
-        assert type(json['text']) is str
+        assert type(json['presigned_url']) is str
         assert type(json['created_at']) is str
         assert type(json['expires_at']) is str
         assert type(json['size_bytes']) is int
 
-        return GetPasteResult(
-            text=json['text'],
+        return GetPasteUrlResult(
+            presigned_url=json['presigned_url'],
             size_bytes=json['size_bytes'],
             created_at_utc=isoparse(json['created_at']),
             expires_at_utc=isoparse(json['expires_at']),
         )
     return _get
 
-@pytest.fixture
-async def api_get_paste_expect_none(service_client):
+@pytest.fixture(scope='session')
+async def s3_get(minio_server) -> bytes:
     async def _get(paste_id: str):
-        response = await service_client.get(f'/api/v1/{paste_id}')
+        response = minio_server['client'].get_object(
+            Bucket=minio_server['bucket'],
+            Key=f"submitted/{paste_id}",
+        )
+        data = response["Body"].read()
+        assert data
+        return data
+    return _get
+
+@pytest.fixture
+async def api_get_paste(api_get_paste_url, s3_get, auth_client):
+    async def _get(paste_id: str, *, client: auth.Client = auth_client) -> GetPasteResult:
+        response = await api_get_paste_url(paste_id, client=client)
+        data = await s3_get(paste_id)
+        return GetPasteResult(
+            data=data,
+            size_bytes=response.size_bytes,
+            created_at_utc=response.created_at_utc,
+            expires_at_utc=response.expires_at_utc,
+        )
+    return _get
+
+@pytest.fixture
+async def api_get_paste_expect_none(auth_client, endpoints):
+    async def _get(paste_id: str, *, client: auth.Client = auth_client):
+        response = await client.get(endpoints['get_paste_presigned_url'] + f'/{paste_id}')
         assert response.status == 404
         assert 'application/json' in response.headers['Content-Type']
         assert response.text == 'null'
--- services/read-service/tests/test_get_paste.py
+++ services/read-service/tests/test_get_paste.py
@@ -1,53 +1,44 @@
 import pytest
 from shared.fixtures.raw_insert_paste import RawInsertResult
+import shared.utils.auth as auth
 
 # =========================================
 # ================= TESTS =================
 # =========================================
-async def test_basic(raw_insert_and_get, mongo_collection):
+async def test_basic(raw_insert_and_get):
     paste_text = 'Hello, world!'
-
-    assert mongo_collection.count_documents({}) == 0
     await raw_insert_and_get(paste_text)
-    assert mongo_collection.count_documents({}) == 1
 
-async def test_utf8(raw_insert_and_get, mongo_collection):
-    paste_text = 'Привет, Андрей!'
-
-    assert mongo_collection.count_documents({}) == 0
+async def test_utf8(raw_insert_and_get):
+    paste_text = 'Привет мир! 🌍 こんにちは'
     await raw_insert_and_get(paste_text)
-    assert mongo_collection.count_documents({}) == 1
 
-async def test_non_existent(api_get_paste_expect_none, mongo_collection):
+async def test_non_existent(api_get_paste_expect_none):
     paste_id = 'abc123'
-
-    assert mongo_collection.count_documents({}) == 0
     await api_get_paste_expect_none(paste_id)
-    assert mongo_collection.count_documents({}) == 0
 
-async def test_expired(api_get_paste_expect_none, raw_insert_paste, mongo_collection):
+async def test_expired(api_get_paste_expect_none, raw_insert_paste):
     paste_id = 'abc123'
     paste_text = 'abc'
 
     await raw_insert_paste(paste_id, paste_text, '-1 second')
-    assert mongo_collection.count_documents({}) == 1
     await api_get_paste_expect_none(paste_id)
 
 # =========================================
 # ============= LOCAL FIXTURES ============
 # =========================================
 @pytest.fixture
-async def raw_insert_and_get(api_get_paste, raw_insert_paste):
-    async def _insert_and_get(paste_text: str) -> RawInsertResult:
+async def raw_insert_and_get(api_get_paste, raw_insert_paste, auth_client):
+    async def _insert_and_get(paste_text: str, *, client: auth.Client = auth_client) -> RawInsertResult:
         paste_id = "abc123"
 
-        raw_insert_res = await raw_insert_paste(paste_id, paste_text)
-        get_response = await api_get_paste(paste_id)
-        
-        assert get_response.created_at_utc == raw_insert_res.pg_created_at_utc
-        assert get_response.expires_at_utc == raw_insert_res.pg_expires_at_utc
-        assert get_response.size_bytes == raw_insert_res.size_bytes
-        assert get_response.text == paste_text
+        raw_insert_res = await raw_insert_paste(paste_id, paste_text, client=client)
+        get = await api_get_paste(paste_id)
+
+        assert get.created_at_utc == raw_insert_res.created_at_utc
+        assert get.expires_at_utc == raw_insert_res.expires_at_utc
+        assert get.size_bytes == raw_insert_res.size_bytes
+        assert get.data == raw_insert_res.data
 
         return raw_insert_res
     return _insert_and_get
