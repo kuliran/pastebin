8359d75837a0328fc236e87ffae760f6a4cce2a5
kuliran <137825720+kuliran@users.noreply.github.com>
2026-02-27 20:14:01 +0300

    fix(ci): add vcpkg root to GITHUB_ENV

--- vcpkg/vcpkg_setup.sh
+++ vcpkg/vcpkg_setup.sh
@@ -3,13 +3,15 @@
 set -e
 
 REPO_ROOT=${1:-/app}
-
-VCPKG_ROOT="/opt/vcpkg"
+VCPKG_ROOT="${VCPKG_ROOT:-/opt/vcpkg}"
+VCPKG_DEFAULT_BINARY_CACHE="${VCPKG_DEFAULT_BINARY_CACHE:-/opt/vcpkg_cache}"
+VCPKG_INSTALLED_DIR="${VCPKG_INSTALLED_DIR:-/opt/vcpkg_installed}"
 export PATH="$VCPKG_ROOT:$PATH"
 export VCPKG_FORCE_SYSTEM_BINARIES=1
 export CC=clang
 export CXX=clang++
 
+mkdir -p $VCPKG_ROOT
 mkdir -p $VCPKG_DEFAULT_BINARY_CACHE
 mkdir -p $VCPKG_INSTALLED_DIR
 sudo chown -R $(id -u):$(id -g) $REPO_ROOT/vcpkg/
