62735f8fa1e3df32e9335f3a70c8b3339428918c
kuliran <137825720+kuliran@users.noreply.github.com>
2026-02-27 19:07:31 +0300

    fix: shared/run_as_user.sh used reserved var name

--- shared/run_as_user.sh
+++ shared/run_as_user.sh
@@ -3,23 +3,23 @@
 # Exit on any error and treat unset variables as errors
 set -euo
 
-UID="$1"
-GID="$2"
+TARGET_UID="$1"
+TARGET_GID="$2"
 shift 2
 
 GROUP_NAME="hostgroup"
 USER_NAME="hostuser"
 
-if ! getent group "$GID" >/dev/null; then
-    groupadd -g "$GID" "$GROUP_NAME"
+if ! getent group "$TARGET_GID" >/dev/null; then
+    groupadd -g "$TARGET_GID" "$GROUP_NAME"
 else
-    GROUP_NAME="$(getent group "$GID" | cut -d: -f1)"
+    GROUP_NAME="$(getent group "$TARGET_GID" | cut -d: -f1)"
 fi
 
-if ! id "$UID" >/dev/null 2>&1; then
-    useradd -m -u "$UID" -g "$GID" "$USER_NAME"
+if ! id "$TARGET_UID" >/dev/null 2>&1; then
+    useradd -m -u "$TARGET_UID" -g "$TARGET_GID" "$USER_NAME"
 else
-    USER_NAME="$(getent passwd "$UID" | cut -d: -f1)"
+    USER_NAME="$(getent passwd "$TARGET_UID" | cut -d: -f1)"
 fi
 
 exec su "$USER_NAME" -c "$*"
