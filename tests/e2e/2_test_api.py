72f5d5b1107e6c3459f73cb54f9d6038daee2a2c
kuliran <137825720+kuliran@users.noreply.github.com>
2026-03-19 12:44:58 +0300

    e2e: test friends

--- tests/e2e/2_test_api.py
+++ tests/e2e/2_test_api.py
@@ -1,35 +1,28 @@
 import pytest
-import time
-from dateutil.parser import isoparse
 from datetime import datetime, timezone
-from shared.utils.client import Client
-from shared.utils.upload_paste import get_paste_id
+from collections import Counter
 
 # ============================================
 async def test_upload_basic(api_upload_paste):
     r = await api_upload_paste("Hello, World!")
     assert len(r.paste_id) > 0
 
-async def test_upload_empty_text_fails(api_upload_create_url_raw, s3_upload, api_upload_submit_raw):
-    create_url = await api_upload_create_url_raw()
-    presigned_url = create_url.json()['presigned_url']
-    paste_id = get_paste_id(presigned_url)
-    await s3_upload(presigned_url, "")
-    r = await api_upload_submit_raw(paste_id)
+async def test_upload_empty_text_fails(api_upload_create_url, s3_upload, api_upload_submit_raw):
+    create_url = await api_upload_create_url()
+    await s3_upload(create_url.presigned_url, "")
+    r = await api_upload_submit_raw(create_url.paste_id)
     assert r.status == 409
 
 async def test_upload_1MB(api_upload_paste):
     text = "x" * (1024 * 1024)
     await api_upload_paste(text)
 
-async def test_upload_too_large_fails(api_upload_create_url_raw, s3_upload, api_upload_submit_raw):
+async def test_upload_too_large_fails(api_upload_create_url, s3_upload, api_upload_submit_raw):
     text = "x" * (1024 * 1024 + 1)
 
-    create_url = await api_upload_create_url_raw()
-    presigned_url = create_url.json()['presigned_url']
-    paste_id = get_paste_id(presigned_url)
-    await s3_upload(presigned_url, text)
-    r = await api_upload_submit_raw(paste_id)
+    create_url = await api_upload_create_url()
+    await s3_upload(create_url.presigned_url, text)
+    r = await api_upload_submit_raw(create_url.paste_id)
     assert r.status == 413
 
 async def test_upload_utf8(api_upload_paste, api_get_paste):
@@ -51,18 +44,16 @@ async def test_unauthorized_upload_fails(api_upload_create_url_raw, new_unauth_c
     r = await api_upload_create_url_raw(client=unauth)
     assert r.status in (400, 401, 403)
 
-async def test_upload_twice_and_get(api_upload_create_url_raw, s3_upload, api_upload_submit_raw, api_get_paste):
+async def test_upload_twice_and_get(api_upload_create_url, s3_upload, api_upload_submit_raw, api_get_paste):
     paste_text = 'Hello, world!'
 
-    create_url = await api_upload_create_url_raw()
-    presigned_url = create_url.json()['presigned_url']
-    paste_id = get_paste_id(presigned_url)
-    first_upload = await s3_upload(presigned_url, paste_text)
-    r = await api_upload_submit_raw(paste_id)
+    create_url = await api_upload_create_url()
+    first_upload = await s3_upload(create_url.presigned_url, paste_text)
+    r = await api_upload_submit_raw(create_url.paste_id)
     assert r.status == 200
-    await s3_upload(presigned_url, 'Other text')
+    await s3_upload(create_url.presigned_url, 'Other text')
 
-    get = await api_get_paste(paste_id)
+    get = await api_get_paste(create_url.paste_id)
     assert get.data == first_upload.data
 
 # ============================================
@@ -213,6 +204,8 @@ async def test_add_rm_friend(api_get_friends, api_add_friend, api_rm_friend, new
     await api_rm_friend(diff._user_id)
     assert len(await api_get_friends()) == 0
 
+    await api_rm_friend(diff._user_id)  # rm twice should work
+
     await add_friend_expect_one()
 
 async def test_rm_non_existent_friend(api_get_friends, api_rm_friend, new_auth_client):
@@ -230,16 +223,18 @@ async def test_many_friends(api_get_friends, api_add_friend, api_rm_friend, new_
 
     # add friends to a diff user
     diff_user = await new_auth_client(f'{username_prefix}diff', f'{username_prefix}1Z')
+    assert len(await api_get_friends(client=diff_user)) == 0
     diff_friends_to_add = []
     for i in range(0, 3):
-        client = new_auth_client(f'{username_prefix}{i+100}', f'{username_prefix}1Z')
-        diff_friends_to_add.append(client)
+        client = await new_auth_client(f'{username_prefix}{i+100}', f'{username_prefix}1Z')
+        diff_friends_to_add.append(client._user_id)
+        await api_add_friend(client._user_id, client=diff_user)
 
     # add our friends
     friends_to_add = []
     for i in range(0, 3):
-        client = new_auth_client(f'{username_prefix}{i}', f'{username_prefix}1Z')
-        friends_to_add.append(client)
+        client = await new_auth_client(f'{username_prefix}{i}', f'{username_prefix}1Z')
+        friends_to_add.append(client._user_id)
 
     assert len(await api_get_friends()) == 0
 
@@ -247,20 +242,14 @@ async def test_many_friends(api_get_friends, api_add_friend, api_rm_friend, new_
         await api_add_friend(friends_to_add[i])
     friends = await api_get_friends()
     assert len(friends) == len(friends_to_add)
-    for i in range(0, len(friends_to_add)):
-        assert friends[i] == friends_to_add[i]
+    assert Counter(friends) == Counter(friends_to_add)
 
 
     # check that friends of a diff user are not affected
     async def check_diff_friends():
-        for i in range(0, len(friends_to_add)):
-            await api_add_friend(diff_friends_to_add[i])
-
         diff_friends = await api_get_friends(client=diff_user)
         assert len(diff_friends) == len(diff_friends_to_add)
-
-        for i in range(0, len(diff_friends_to_add)):
-            assert diff_friends[i] == diff_friends_to_add[i]
+        assert Counter(diff_friends) == Counter(diff_friends_to_add)
     await check_diff_friends()
 
 
@@ -270,26 +259,95 @@ async def test_many_friends(api_get_friends, api_add_friend, api_rm_friend, new_
 
         friends_left = await api_get_friends()
         assert len(friends_left) == len(friends_to_add)
-        for j in range(0, len(friends_to_add)):
-            friends_left[j] == friends_to_add[j]
+        assert Counter(friends_left) == Counter(friends_to_add)
 
     await check_diff_friends()
 
+    # check that adding same friends changes nothing
+    for i in range(0, len(diff_friends_to_add)):
+        await api_add_friend(diff_friends_to_add[i])
+
+    await check_diff_friends()
+
+
 async def test_friends_visibility(
-        api_add_friend, api_rm_friend,
-        api_upload_paste, new_auth_client,
-        api_get_paste, api_get_paste_expect_unauth
+    api_add_friend, api_rm_friend, auth_client,
+    api_upload_paste, new_auth_client,
+    api_get_paste, api_get_paste_expect_unauth
 ):
     diff = await new_auth_client('test_friends_visibility', 'test_friends_visibility')
-    
-    upload = await api_upload_paste('some text')
 
-    await api_get_paste_expect_unauth(upload.paste_id, client=diff._user_id)
+    upload = await api_upload_paste('some text', visibility='friends')
+
+    await api_get_paste_expect_unauth(upload.paste_id, client=diff)
     await api_add_friend(diff._user_id)
-    get = await api_get_paste(upload.paste_id, client=diff._user_id)
+    get = await api_get_paste(upload.paste_id, client=diff)
     assert get.data == upload.data
 
-    await api_rm_friend(diff._user_id)
-    await api_get_paste_expect_unauth(upload.paste_id, client=diff._user_id)
+    # Friendship is unidirectional
+    diff_upload = await api_upload_paste('some diff text', visibility='friends', client=diff)
+    await api_get_paste_expect_unauth(diff_upload.paste_id)
 
-# TODO friend limit
+    await api_add_friend(auth_client._user_id, client=diff)
+    get = await api_get_paste(diff_upload.paste_id)
+    assert get.data == diff_upload.data
+
+    await api_rm_friend(diff._user_id)
+    await api_get_paste_expect_unauth(upload.paste_id, client=diff)
+    await api_get_paste(diff_upload.paste_id)
+
+async def test_friend_limit(api_get_friends, api_add_friend, api_add_friend_raw, api_rm_friend, new_auth_client, auth_client):
+    username_prefix = 'test_friend_limit_'
+    LIMIT = 5
+
+    all_users = []
+    for i in range(0, LIMIT+1):
+        client = await new_auth_client(f'{username_prefix}{i}', f'{username_prefix}1Z')
+        all_users.append(client._user_id)
+    friends_to_add = all_users[:-1]
+
+    assert len(await api_get_friends()) == 0
+
+    for i in range(0, len(friends_to_add)):
+        await api_add_friend(friends_to_add[i])
+
+    async def check_friends():
+        friends = await api_get_friends()
+        assert len(friends) == len(friends_to_add)
+        assert Counter(friends) == Counter(friends_to_add)
+    await check_friends()
+
+    # adding an already-friend at the limit should not work
+    r = await api_add_friend_raw(all_users[0])
+    assert r.status == 409
+    await check_friends()
+
+    # adding a new friend at the limit should not work
+    r = await api_add_friend_raw(all_users[-1])
+    assert r.status == 409
+    await check_friends()
+
+    # removing a non-existent friend should not change limit
+    await api_rm_friend(all_users[-1])
+    r = await api_add_friend_raw(all_users[-1])
+    assert r.status == 409
+    await check_friends()
+
+    # removing a friend should change limit
+    idx_to_rm = len(friends_to_add)//2
+    await api_rm_friend(friends_to_add[idx_to_rm])
+    friends_to_add.remove(friends_to_add[idx_to_rm])
+    await check_friends()
+
+    # user should now be able to add a new friend
+    await api_add_friend(all_users[-1])
+    friends_to_add.append(all_users[-1])
+    await check_friends()
+
+    # we have reached the friend limit again
+    new_client = await new_auth_client(f'{username_prefix}{LIMIT+100}', f'{username_prefix}1Z')
+    r = await api_add_friend_raw(new_client._user_id)
+    assert r.status == 409
+
+    # but other clients should be able to add friends regardless of others' limit
+    await api_add_friend(auth_client._user_id, client=new_client)
--- tests/e2e/fixtures/api_upload_paste.py
+++ tests/e2e/fixtures/api_upload_paste.py
@@ -1,6 +1,23 @@
 import pytest
+from dataclasses import dataclass
 from shared.utils.upload_paste import *
 from shared.utils.client import Client
+
+@dataclass
+class CreateUrlResult:
+    presigned_url: str
+    paste_id: str
+
+@pytest.fixture
+def api_upload_create_url(api_upload_create_url_raw, auth_client):
+    async def _impl(*, client: Client = auth_client) -> CreateUrlResult:
+        create_url = await api_upload_create_url_raw(client=client)
+        json = create_url.json()
+        return CreateUrlResult(
+            presigned_url=json['presigned_url'],
+            paste_id=json['paste_id']
+        )
+    return _impl
 
 @pytest.fixture
 def api_upload_paste(api_upload_create_url_raw, s3_upload, api_upload_submit_raw, auth_client):
@@ -17,10 +34,10 @@ def api_upload_paste(api_upload_create_url_raw, s3_upload, api_upload_submit_raw
         assert create_url.status == 201
         json = create_url.json()
         presigned_url = json['presigned_url']
-        paste_id = get_paste_id(presigned_url)
+        paste_id = json['paste_id']
         
         upload = await s3_upload(presigned_url, paste_text)
-        submit = await api_upload_submit_raw(paste_id)
+        submit = await api_upload_submit_raw(paste_id, client=client)
         assert submit.status == 200
 
         return UploadPasteResult(
